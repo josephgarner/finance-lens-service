3a2691a8481cb245160e0aef676a257ea22652bd
Joseph Garner <joseph.peter.garner@gmail.com>
2022-11-11 09:09:07 +1100

    Protected all endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import Koa from "koa";
 require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 import bodyParser from "koa-bodyparser";
+import jwt from "koa-jwt";
 import { connectToDatabase } from "./db";
 import { rootRouter } from "./routes";
+import jwtrsa from "jwks-rsa";
+import { env } from "process";
 
 const cors = require("@koa/cors");
 const logger = require("koa-logger");
@@ -12,11 +15,36 @@ console.log("ENV:", process.env.NODE_ENV);
 connectToDatabase();
 
 // run app
+app.use(cors());
+app.use(
+  jwt({
+    secret: jwtrsa.koaJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: process.env.JWKSURI || "",
+    }),
+    audience: process.env.AUDIENCE,
+    issuer: process.env.ISSUER,
+    cookie: "Authorization",
+    algorithms: ["RS256"],
+  })
+);
+
+app.use((ctx, next) => {
+  return next().catch((err) => {
+    if (401 == err.status) {
+      ctx.status = 401;
+      ctx.body = "Protected resource, use Authorization header to get access\n";
+    } else {
+      throw err;
+    }
+  });
+});
 app.use(bodyParser());
+app.use(logger());
 app.use(rootRouter.routes());
 app.use(rootRouter.allowedMethods());
-app.use(logger());
-app.use(cors());
 
 app.on("error", function (error) {
   console.log("Server has thrown and error");
