9de93f04b4a1c4bf97932a06040768318009a8bd
Joseph Garner <joseph.peter.garner@gmail.com>
2022-12-02 12:54:06 +1100

    Patch for retrieving categories

--- src/dal/transaction/listAllCategories.ts
+++ src/dal/transaction/listAllCategories.ts
@@ -2,17 +2,19 @@ import { transactionData } from "../../db";
 
 export const listAllCategories = async (userID: string) => {
   console.log("query");
-  const categories = await transactionData
+  let categories = await transactionData
     .find({ userID: userID })
     .select("category")
     .distinct("category")
     .exec();
 
-  const subcategories = await transactionData
+  let subcategories = await transactionData
     .find({ userID: userID })
     .select("subcategory")
     .distinct("subcategory")
     .exec();
+  subcategories = subcategories.filter((e) => e !== null || e !== "");
+  categories = categories.filter((e) => e !== null || e !== "");
   console.log(categories, subcategories);
   return { categories, subcategories };
 };
